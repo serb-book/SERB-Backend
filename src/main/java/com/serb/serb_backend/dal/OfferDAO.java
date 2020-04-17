10834a012166af2a2099c6eda627c1b48337ff4b
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-04-17 15:51:29 +0200

    add offer interface

--- src/main/java/com/serb/serb_backend/dal/OfferDAO.java
+++ src/main/java/com/serb/serb_backend/dal/OfferDAO.java
@@ -7,6 +7,22 @@
  */
 package com.serb.serb_backend.dal;
 
-public interface OfferDAO {
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.serb.serb_backend.dto.OfferDTO;
+
+public interface OfferDAO {
+	/**
+	 * @param offer
+	 * @return if operation was done successfully
+	 */
+	boolean addOffer(OfferDTO offer);
+	
+	/**
+	 * takes an excel file containing offer data then start 
+	 * storing them
+	 * @param excelFile
+	 * @return
+	 */
+	boolean addOffersFromExcelFile(XSSFWorkbook excelFile);
 }
