e03b35055924c38b4c0b127c06c63d1f521296db
abdo ragab hashem <ar1813@fayoum.edu.eg>
2020-04-28 18:00:21 +0200

    fix: add main enviroment

--- src/main/java/com/serb_backend/SerbBackendApplication.java
+++ src/main/java/com/serb_backend/SerbBackendApplication.java
@@ -3,6 +3,7 @@ package com.serb_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SerbBackendApplication {
