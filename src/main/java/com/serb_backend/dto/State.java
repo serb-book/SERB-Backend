e5baad34921d9e949f6732e9d1d212aa7ee9d5a4
abdo ragab hashem <ar1813@fayoum.edu.eg>
2020-04-28 18:32:00 +0200

    fix: convert state images to array
     Changes to be committed:
    	modified:   src/main/java/com/serb_backend/dto/State.java

--- src/main/java/com/serb_backend/dto/State.java
+++ src/main/java/com/serb_backend/dto/State.java
@@ -2,26 +2,36 @@ package com.serb_backend.dto;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+
+import static io.qala.datagen.RandomShortApi.*;
 import com.github.javafaker.Faker;
 
 @Data
 class State {
 	// FIXME no images in database for offer table
-	// NOTE should't this be array of images ?
-	private String image;
+	private ArrayList<String> images;
 	private String text;
 	public State() {}
-	public State(String image, String text) {
+	public State(ArrayList<String> images, String text) {
 		super();
-		this.image = image;
+		this.images = images;
 		this.text = text;
 	}
 
 	public static State random(){
 		Faker faker = new Faker();
 		State state = new State();
-		
-		state.image = "https://picsum.photos/200/300?random=1";
+		state.images =new ArrayList<String>();
+		if (weighedTrue(.7)){
+			state.images.add("https://picsum.photos/200/300?random=1"); 
+			if (bool()){
+				state.images.add("https://picsum.photos/200/300?random=1"); 
+			}
+			if (bool()){
+				state.images.add("https://picsum.photos/200/300?random=1"); 
+			}
+		}
 		state.text = faker.lorem().paragraph();
 
 		return state;
